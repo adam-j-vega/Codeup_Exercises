cd2875a4ade86c97315892744ed0d0b98be00f74
Adam Vega <adam.j.vega@hotmail.com>
2014-08-19 11:56:32 -0500

    initial commit

---
+++ internal_functions.php
@@ -0,0 +1,30 @@
+<?php
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+function is_set_or_empty($variable) {
+	if(!empty($variable)) {
+		echo 'variable is FULL';
+	}
+		else {
+			echo 'variable is EMPTY';
+		}
+}
+
+$serialize_array = serialize($array);
+// var_dump($serialize_array);
+// var_dump($array);
+echo $serialize_array;
+
+echo is_set_or_empty($nothing) . PHP_EOL;
+echo is_set_or_empty($something) . PHP_EOL;
+
+
+$unserialize_array=unserialize($serialize_array);
+
+var_dump($unserialize_array)
+
+
+
+?>
