358dc65b9383eea5a118a78f13f3f8d1aba73507
Adam Vega <adam.j.vega@hotmail.com>
2014-09-08 20:35:26 -0500

    initial commit

--- arithmetic.php
+++ arithmetic.php
@@ -1,35 +1,47 @@
 <?php
 
+//arithmetic exercises in php
+
 $a=5;
 $b=0;
 
-function dividing_zero_error($a) {
+function dividing_zero_error($a) 
+{
 	return $dividing_by_zero=false;
 	}
-function error($error) {
+function error($error) 
+{
 		return $error=false;
 	}
-function add($a, $b) {
+function add($a, $b) 
+{
 		return $a + $b;
 }
-function subtract($a, $b) {
+function subtract($a, $b) 
+{
 		return $a - $b;
 }
-function multiply($a, $b) {
+function multiply($a, $b) 
+{
 		return $a * $b;
 }
-function divide($a, $b) {
+function divide($a, $b) 
+{
 		return $a + $b;
 }
-function modulus($a, $b) {
+function modulus($a, $b) 
+{
 		return $a + $b;
 }				
 
-if(!is_numeric($a) || !is_numeric($b)) {
+if(!is_numeric($a) || !is_numeric($b)) 
+{
 	error(true);
-}elseif($a==0 || $b==0) {
+}elseif($a==0 || $b==0) 
+{
 	echo dividing_zero_error(true);
-}else{
+}else
+{
 	add($a,$b);
 	subtract($a,$b);
 	multiply($a,$b);
