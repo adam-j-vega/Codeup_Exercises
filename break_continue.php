fbf9dcb03bff8c0f558e37406f95c1ba581eb75f
Adam Vega <adam.j.vega@hotmail.com>
2014-09-08 20:29:06 -0500

    initial commit

--- break_continue.php
+++ break_continue.php
@@ -1,22 +1,20 @@
 <?php
-
-
-
 //    Create a for loop that shows all even numbers between 1 and 100 using continue.
 
 //    Create another for loop that counts from 1 to 100, but stops after 10 using break.
 
+//===============prior iteration==================
 // for( $num=2 ; $num<=100 ; $num+=2 ){
 // 	echo "{$num} \n";
 // 	continue;
 // }
-
-for($num=1 ; $num<=100 ; ++$num){
+//===============final iteration==================
+for($num=1 ; $num<=100 ; ++$num)
+{
 	echo "{$num} \n";
-		if($num==10){
+		if($num==10)
+		{
 		break;
 	}
 }
-
-
 ?>
