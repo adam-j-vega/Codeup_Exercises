5383fc883116fedf47e6df0ada2c82b7440914a7
Adam Vega <adam.j.vega@hotmail.com>
2014-09-08 20:39:04 -0500

    initial commit

--- search_arrays.php
+++ search_arrays.php
@@ -1,5 +1,7 @@
 <?php
 
+//array search exercise
+
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
@@ -8,27 +10,27 @@ $result = array_intersect($names, $compare);
 
 print_r ($result);
 
-// generating a pre-existing function is stupid.
-// function compare_two_arrays ($array_1, $array_2) {
+function compare_two_arrays ($array_1, $array_2) 
+{
 
-// $array_comparison= array_intersect($array_1, $array_2);
+	$array_comparison= array_intersect($array_1, $array_2);
 
-// return $result=$array_comparison;
+	return $result=$array_comparison;
 
-// }
+}
 
-// $array_1=$names;
-// $array_2=$compare;
-// compare_two_arrays ($array_1, $array_2);
-// $result=$print_result;
-// var_dump($print_result);
+$array_1=$names;
+$array_2=$compare;
+compare_two_arrays ($array_1, $array_2);
+$result=$print_result;
+var_dump($print_result);
 
 
-// $array_1_test = $names[1];
-// $array_2= $compare;
-// compare_two_arrays($array_1,$array_2);
-// $names++
-// $array_1_test = $names[2];
+$array_1_test = $names[1];
+$array_2= $compare;
+compare_two_arrays($array_1,$array_2);
+$names++
+$array_1_test = $names[2];
 
 
 ?>
